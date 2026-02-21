807348de312e4228406e688cd571606b1d7903e2
sabbir185 <sabbir.cse@yahoo.com>
2026-02-21 13:58:43 +0600

    create_jwt basic done

--- utils/create_jwt.go
+++ utils/create_jwt.go
@@ -13,6 +13,7 @@ type Header struct {
 }
 
 type Payload struct {
+	Sub         int    `json:"sub"`
 	FirstName   string `json:"first_name"`
 	LastName    string `json:"last_name"`
 	Email       string `json:"email"`
@@ -56,3 +57,20 @@ func CreateJWT(secret string, data Payload) (string, error) {
 func Base64UrlEncode(data []byte) string {
 	return base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString(data)
 }
+
+
+/*
+
+	jwt, err := utils.CreateJWT("my-secret", utils.Payload{
+		Sub:         1,
+		FirstName:   "Sabbir",
+		LastName:    "Ahmmed",
+		Email:       "sabbir.cse.nub@gmail.com",
+		IsShopOwner: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+	println(jwt)
+
+*/
